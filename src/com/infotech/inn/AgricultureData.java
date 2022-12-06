94441428e0c6e651d81f9f703b4d5420ca3a8831
ChanduTec <108215122+ChanduTec@users.noreply.github.com>
2022-12-06 21:01:50 +0530

    Update AgricultureData.java
    I added 2 lines of codedd

--- src/com/infotech/inn/AgricultureData.java
+++ src/com/infotech/inn/AgricultureData.java
@@ -3,5 +3,8 @@ package com.infotech.inn;
 public class AgricultureData {
   public void agg() {
 	  System.out.println("I am in agg() in Agriculture class");
+	  System.out.println("I am in agg() in Agriculture class");
+	  System.out.println("I am in agg() in Agriculture class");
+	  System.out.println("welcome chandu velagapudi");
   }
 }
